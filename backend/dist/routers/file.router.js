e9efd3b13f7e94fb603a16e45acaeded99e27135
Emir Göçük <51445495+emirgocuk@users.noreply.github.com>
2025-08-24 23:26:14 +0300

    düzenleme

--- backend/dist/routers/file.router.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const file_controller_1 = require("../controllers/file.controller");
-const router = (0, express_1.Router)();
-router.get("/cover/:id", file_controller_1.showCover);
-exports.default = router;
